d9b5174144228af892a8d24e3cabcda96f361a9c
MichaelRay2 <michaelray2809@yahoo.com>
2024-05-04 11:39:04 +0100

    save

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Defining the user's board and the Computer's board
+
+user_board = [[' '] * 10 for i in range(10)]
+computer_board = [[' '] * 10 for i in range(10)]
+
+letters_to_numbers = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9}
+
+def print_board():
+    print("A B C D E F G")
+    print("-------------")
+    row_number = 1
+
+def get_ship_location():
+    pass
+
+def count_hit_ships():
+    pass
+
+create_ships()
+turns = 10
+while turns > 0:
+    
+
+    
